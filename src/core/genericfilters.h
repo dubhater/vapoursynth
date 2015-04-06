8e44d7104622eac05b55cbcc6dcb218a0d8c206f
cantabile <cantabile.desu@gmail.com>
2015-04-06 12:43:01 +0300

    std: Add most of GenericFilters
    Written from scratch.

---
+++ src/core/genericfilters.h
@@ -0,0 +1,10 @@
+
+
+#ifndef GENERICFILTERS_H
+#define GENERICFILTERS_H
+
+#include "VapourSynth.h"
+
+void VS_CC genericInitialize(VSConfigPlugin configFunc, VSRegisterFunction registerFunc, VSPlugin *plugin);
+
+#endif // GENERICFILTERS_H
